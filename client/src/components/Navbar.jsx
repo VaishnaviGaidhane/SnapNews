55790aa9300ff2ff7ba6442bae84865caf0eb813
VaishnaviGaidhane <vaishnavinavanathgaaidhane@gmail.com>
2025-09-04 10:03:14 +0530

    Add all project files

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 
 
-const Navbar = () => {
+const Navbar = ({setCategory}) => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
@@ -10,26 +10,27 @@ const Navbar = () => {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <div className="nav-link" onClick={()=>setCategory("technology")}>Technology</div>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+          <div className="nav-link" onClick={()=>setCategory("business")}>Business</div>
         </li>
+        <li className="nav-item">
+          <div className="nav-link" onClick={()=>setCategory("health")}>Health</div>
+        </li>
+        <li className="nav-item">
+          <div className="nav-link" onClick={()=>setCategory("science")}>Science</div>
+        </li>
+        <li className="nav-item">
+          <div className="nav-link" onClick={()=>setCategory("sports")} >Sports</div>
+        </li>
+        <li className="nav-item">
+          <div className="nav-link" onClick={()=>setCategory("entertainment")}>Entertainment</div>
+        </li>
+
+        
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
